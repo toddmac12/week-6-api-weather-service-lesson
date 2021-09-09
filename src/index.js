e90a9e672023729dd3e7b28e41074e2128f84b21
toddmac12 <toddmac12@gmail.com>
2021-09-09 14:23:56 -0400

    initial commit of project files

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import WeatherService from './weather-service.js';
+
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
+
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    clearFields();
+    WeatherService.getWeather(city)
+      .then(function(response) {
+        getElements(response);
+      });
+  });
+});
+
